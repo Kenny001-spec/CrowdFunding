78c5a2075037d377044ef4433c67609f267c3c83
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-02-19 11:41:11 +0100

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,14 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 // import { StateContextProvider } from './context';
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ThirdwebProvider desiredChainId={ChainId.sepolia}>
     <Router>
-        <App />
+      <App />
     </Router>
   </ThirdwebProvider>
 );
